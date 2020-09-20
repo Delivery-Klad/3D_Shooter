4c0dd30c856077e0f8beb245c373041a512ae126
Danila <dakfadeev@gmail.com>
2020-09-20 17:30:36 +0300

    add shadow settings, bake daylight's city light

---
+++ Assets/FLEX/Scripts/Map_Scripts/LoadShadows.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadShadows : MonoBehaviour
+{
+    [SerializeField] Light MainLight;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Shadows"))
+        {
+            int index = PlayerPrefs.GetInt("Shadows");
+            if (index == 0)
+            {
+                MainLight.shadows = LightShadows.None;
+            }
+            else if (index == 1)
+            {
+                MainLight.shadows = LightShadows.Soft;
+            }
+            else if (index == 2)
+            {
+                MainLight.shadows = LightShadows.Hard;
+            }
+        }
+        if (PlayerPrefs.HasKey("ShadowsQuality"))
+        {
+            int index = PlayerPrefs.GetInt("ShadowsQuality");
+            if (index == 0)
+            {
+                MainLight.shadowResolution = (UnityEngine.Rendering.LightShadowResolution)ShadowResolution.Low;
+            }
+            else if (index == 1)
+            {
+                MainLight.shadowResolution = (UnityEngine.Rendering.LightShadowResolution)ShadowResolution.Medium;
+            }
+            else if (index == 2)
+            {
+                MainLight.shadowResolution = (UnityEngine.Rendering.LightShadowResolution)ShadowResolution.High;
+            }
+            else if (index == 3)
+            {
+                MainLight.shadowResolution = (UnityEngine.Rendering.LightShadowResolution)ShadowResolution.VeryHigh;
+            }
+        }
+    }
+}
